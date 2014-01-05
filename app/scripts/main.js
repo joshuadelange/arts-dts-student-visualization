d57157b237ef1d0024acf7f4b49a1b6ef378905d
Joshua de Lange <joshua.delange@gmail.com>
2014-01-05 16:12:20 +0100

    moar peeps + moar awesomeness!

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,38 +2,63 @@
 
 var L = L,
     google = google,
-    popups = [] ;
+    markers = [] ;
 
 $(document).ready(function(){
 
-    
-    console.log('sup') ;
+    var nextMarker = function(i){
+
+        markers[i].closePopup() ;
+
+        map.setView(markers[i].getLatLng(), 5) ;
+
+        i++;
+        if(i === 21) {
+            //because there is a bug i cant fix last moment
+            window.reload() ;
+        }
+
+        setTimeout(function(){
+
+            map.setView(markers[i].getLatLng(), 3) ;
+
+            setTimeout(function(){
+
+                map.setView(markers[i].getLatLng(), 5) ;
+
+                setTimeout(function(){
+        
+                    markers[i].openPopup() ;
+                    map.setView(markers[i].getLatLng(), 8) ;                   
+
+                    setTimeout(function(){
+                        nextMarker(i);
+                    }, 5000) ;
+
+                }, 1000) ;
+
+            }, 1000) ;
+
+        }, 1000) ;
+
+    } ;
 
     // var map = L.map('map').setView([51.923943445544715, 5.1416015625], 7);
-    var map = L.map('map').setView([52.3731, 4.8922], 3),
+    var map = L.map('map').setView([52.3731, 4.8922], 4),
         labels = {
-            'Accepted': 'label-success',
-            'Process': 'label-warning',
-            'Praying': 'label-default'
+            'Student': 'label-success',
+            'Staff': 'label-info',
+            'Part-time staff': 'label-info',
+            'School Leader': 'label-info'
         } ;
-
-    map.on('zoomend', function(){
-            console.log('end of zoom') ;
-            $.each(popups, function(index, popup){
-                popup.openPopup() ;
-            }) ;
-        }
-    ) ;
 
     L.tileLayer('http://{s}.tile.cloudmade.com/8E10386EF81C4270B374C76464C939C2/113231/256/{z}/{x}/{y}.png', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
     }).addTo(map);
 
+    $.getJSON('https://spreadsheets.google.com/feeds/cells/0ApT5f0KS7hjVdEJIVGVMUWdiNlQzbTBxMDVPSlVzb3c/od6/public/basic?alt=json-in-script&callback=?', function(r){
 
-
-    $.getJSON('https://spreadsheets.google.com/feeds/cells/0ApT5f0KS7hjVdGs3Y1FMMV8xdlFYc0R6M2hRa1ZkRUE/od6/public/basic?alt=json-in-script&callback=?', function(r){
-
-        var numOfColumns = 5,
+        var numOfColumns = 7,
             lists = [],
             data = {} ;
 
@@ -54,40 +79,27 @@ $(document).ready(function(){
             data[value.shift().replace(/ /g, '_').toLowerCase()] = value ;
         }) ;
 
-        console.log(data) ;
-
         $.each(data.name, function(index, value){
 
-            console.log('checking for', value, index) ;
-
-            var geocoder = new google.maps.Geocoder(),
-                address = data.city[index] + ', ' + data.country[index] ;
-
-            geocoder.geocode({
-                'address': address
-            }, function(results, status) {
-
-                if (status === google.maps.GeocoderStatus.OK) {
-
-                    var lat = results[0].geometry.location.lat(),
-                        lon = results[0].geometry.location.lng() ;
-
-                    var marker = L.marker([lat, lon], {
-                        icon: L.divIcon({
-                            className: 'icon',
-                            html: '<img src="' + data.photo[index] + '"><p>' + value + '<span class="label ' + labels[data.status[index]] + '">' + data.status[index] + '</span></p>'
-                        }),
-
-                    }).addTo(map) ;
-                
+            var marker = L.marker([data.lat[index], data.lon[index]]).bindPopup(
+                '<img src="' + data.photo[index] + '"><p>' + value + '<span class="label label-inverse">' + data.role[index] + '</span></p><p class="muted">' + data.city[index] + ', ' + data.country[index] + '</p>',
+                {
+                    offset: [0, 0],
+                    autoPanPaddingTopLeft: 10,
+                    autoPanPaddingBottomRight: 10
                 }
+            ).openPopup() ;
 
-        });
+            markers.push(marker) ;
+            map.addLayer(marker) ;
+
         }) ;
 
+        nextMarker(0) ;
+
+        // markers[0].openPopup() ;
+        // map.setView(markers[0].getLatLng(), 6) ;
+
     }) ;
-
-
-
 
 }) ;
